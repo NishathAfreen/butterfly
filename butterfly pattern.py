11f5e330678509a38ae026e630814b55aaecd4ad
nishath@25 <65917580+NishathAfreen@users.noreply.github.com>
2020-06-26 00:00:16 +0530

    Create butterfly pattern.py

---
+++ butterfly pattern.py
@@ -0,0 +1,18 @@
+
+n=int(input())
+for i in range(0,n):
+      for j in range(0,(i+1)):
+            print("*",end=" ")
+      for j in range(2*(n-(i+1)),0,-1):
+            print(" ",end=" ")
+      for j in range(0,i+1):
+            print("*",end=" ")
+      print()
+for i in range(n-2,-1,-1):
+      for j in range(i+1,0,-1):
+            print("*",end=" ")
+      for j in range(0,2*(n-(i+1))):
+            print(" ",end=" ")
+      for j in range(i+1,0,-1):
+            print("*",end=" ")
+      print()
